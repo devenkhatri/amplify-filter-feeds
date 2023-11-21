586069ffeb1a5c12162ee0a8234194c17dddbf53
Deven Goratela <deven.goratela@tcs.com>
2023-11-21 21:28:51 +0530

    layout finalized

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import './App.css'
 import Example from './components/Example'
-import Lay from './components/Lay'
 import Layout from './components/Layout'
 
 function App() {
 
   return (
     <>
-    <Lay />
-      {/* <Layout /> */}
+      <Layout />
       {/* <Example />       */}
     </>
   )
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -4,10 +4,13 @@ import {
   Card,
   DropZone,
   Flex,
+  Grid,
   Heading,
+  Input,
   Text,
   View,
   VisuallyHidden,
+  useTheme,
 } from "@aws-amplify/ui-react";
 
 const Layout = () => {
@@ -21,12 +24,41 @@ const Layout = () => {
     }
     setFiles(Array.from(files));
   };
-
+  const { tokens } = useTheme();
   return (
-    <View width="100%" maxWidth="50rem" padding={{ base: 0, large: "2rem" }}>
-      <Card variation="elevated">
-        <Flex direction={"column"} gap={"1rem"}>
-          <Heading level={5}>Input Section</Heading>
+    <Card variation="elevated" style={{ width: "100vh" }}>
+      <Grid
+        columnGap="0.5rem"
+        rowGap="0.5rem"
+        templateColumns="1fr 1fr 1fr"
+        templateRows="1fr 3fr 1fr"
+      >
+        <Card
+          backgroundColor={tokens.colors.primary[80]}
+          color={tokens.colors.white}
+          columnStart="1"
+          columnEnd="-1"
+        >
+          <Flex
+            direction="column"
+            justifyContent="center"
+            alignItems="center"
+            alignContent="center"
+            wrap="nowrap"
+            gap="1rem"
+          >
+            <Heading level={1} color={tokens.colors.white}>
+              Filter Feeds
+            </Heading>
+          </Flex>
+        </Card>
+        <Card columnStart="1" columnEnd="-1">
+          <View as="p" marginBottom="1rem">
+            <Heading level={3} color={tokens.colors.primary[90]}>
+              Input Section
+            </Heading>
+          </View>
+
           <DropZone
             acceptedFileTypes={acceptedFileTypes}
             onDropComplete={({ acceptedFiles, rejectedFiles }) => {
@@ -53,9 +85,58 @@ const Layout = () => {
           {files.map((file) => (
             <Text key={file.name}>{file.name}</Text>
           ))}
-        </Flex>
-      </Card>
-    </View>
+          <Flex direction="column" margin="1rem 0">
+            <Text>Keywords : </Text>
+            <Input placeholder="Add comma seperated keywords" />
+          </Flex>
+          <Flex
+            direction="row"
+            justifyContent="space-between"
+            alignItems="center"
+            alignContent="space-between"
+            wrap="nowrap"
+            gap="1rem"
+          >
+            <Button
+              variation="primary"
+              loadingText=""
+              onClick={() => alert("WIP")}
+            >
+              Process
+            </Button>
+            {/* <View
+              as="progress"
+              data-progress-bar
+              // color={tokens.colors.primary[90]}
+              padding="1rem"
+              max="100"
+              value="75"
+            /> */}
+          </Flex>
+        </Card>
+        <Card
+          backgroundColor={tokens.colors.primary[10]}
+          columnStart="1"
+          columnEnd="-1"
+        >
+          <View as="p" marginBottom="1rem">
+            <Heading level={3} color={tokens.colors.primary[90]}>
+              Output Section
+            </Heading>
+          </View>
+          <Text>The generated excel file can be downloaded from here once the process is completed</Text>
+          <Button
+            variation="primary"
+            // colorTheme="info"
+            loadingText=""
+            disabled
+            onClick={() => alert("hello")}
+          >
+            Download
+          </Button>
+        </Card>
+      </Grid>
+    </Card>
   );
 };
 
